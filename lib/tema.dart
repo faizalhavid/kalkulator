d4b1995db3d531150c48a4f05491f55c9743e958
faizalhavid <67695722+faizalhavid@users.noreply.github.com>
2023-05-01 21:31:24 +0700

    Update and rename theme.dart to tema.dart

--- lib/tema.dart
+++ lib/tema.dart
@@ -6,7 +6,8 @@ var red = Color.fromARGB(255, 255, 32, 36);
 var amber = const Color(0xFFE19798);
 var cyan = const Color(0xFF29D7DF);
 var white = const Color(0xFFFFFFFF);
-//FONT WEIGHT
+
+
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
